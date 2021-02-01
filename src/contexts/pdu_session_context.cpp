008acaf75c568f5463f81f760f77e7feb640d8a0
Tien-Thinh Nguyen <Tien-Thinh.Nguyen@eurecom.fr>
2021-02-01 15:49:02 +0100

    Apply clang-format for AMF Contexts

--- src/contexts/pdu_session_context.cpp
+++ src/contexts/pdu_session_context.cpp
@@ -3,9 +3,9 @@
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The OpenAirInterface Software Alliance licenses this file to You under
- * the OAI Public License, Version 1.1  (the "License"); you may not use this file
- * except in compliance with the License.
- * You may obtain a copy of the License at
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this
+ *file except in compliance with the License. You may obtain a copy of the
+ *License at
  *
  *      http://www.openairinterface.org/?page_id=698
  *
@@ -30,11 +30,10 @@
 
 //------------------------------------------------------------------------------
 pdu_session_context::pdu_session_context() {
-  smf_available = false;
+  smf_available    = false;
   isn2sm_avaliable = false;
   isn1sm_avaliable = false;
 }
 
 //------------------------------------------------------------------------------
-pdu_session_context::~pdu_session_context() {
-}
+pdu_session_context::~pdu_session_context() {}
--- src/contexts/ue_context.hpp
+++ src/contexts/ue_context.hpp
@@ -3,9 +3,9 @@
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The OpenAirInterface Software Alliance licenses this file to You under
- * the OAI Public License, Version 1.1  (the "License"); you may not use this file
- * except in compliance with the License.
- * You may obtain a copy of the License at
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this
+ *file except in compliance with the License. You may obtain a copy of the
+ *License at
  *
  *      http://www.openairinterface.org/?page_id=698
  *
@@ -41,8 +41,8 @@ using namespace ngap;
 
 class ue_context {
  public:
-  uint32_t ran_ue_ngap_id;  //32bits
-  long amf_ue_ngap_id :40;  //40bits
+  uint32_t ran_ue_ngap_id;   // 32bits
+  long amf_ue_ngap_id : 40;  // 40bits
 
   e_Ngap_RRCEstablishmentCause rrc_estb_cause;
   bool isUeContextRequest;
